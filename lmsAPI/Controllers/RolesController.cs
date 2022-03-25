fe2bb42126459f728ab41c86a2d1485bfdd3555e
firstdeyan <firstdeyan.septiana.tif19@polban.ac.id>
2022-03-25 21:46:04 +0700

    Initial commit

--- lmsAPI/Controllers/RolesController.cs
+++ lmsAPI/Controllers/RolesController.cs
@@ -7,7 +7,7 @@ namespace lmsAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Roles = "admin")]
+    [Authorize(Roles = "admin,superadmin")]
     [EnableCors]
 
     public class RolesController : ControllerBase
