96741b2a0cd21ec78a33ddfda2cfa879f3ec9e21
firstdeyan <firstdeyan.septiana.tif19@polban.ac.id>
2022-03-26 01:22:08 +0700

    Initial commit

--- lmsAPI/login.cs
+++ lmsAPI/login.cs
@@ -1,7 +1,10 @@
-﻿namespace lmsAPI
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lmsAPI
 {
     public class login
     {
+        [EmailAddress(ErrorMessage = "Format email salah")]
         public string email { get; set; } = string.Empty;
         public string password { get; set; } = string.Empty;
     }
