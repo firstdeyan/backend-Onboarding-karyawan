cd157e2ed89f4b1c834524a2ccf4c72a71e89b1b
firstdeyan <firstdeyan.septiana.tif19@polban.ac.id>
2022-03-26 01:23:37 +0700

    Initial commit

---
+++ lmsAPI/Controllers/editPasswordSucces.cs
@@ -0,0 +1,9 @@
+﻿namespace lmsAPI.Controllers
+{
+    internal class editPasswordSucces
+    {
+        public string Status { get; internal set; }
+        public string Code { get; internal set; }
+        public string Message { get; set; }
+    }
+}
