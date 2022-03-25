0d4f111a13eae34962c3452cbe357357b21ae697
firstdeyan <firstdeyan.septiana.tif19@polban.ac.id>
2022-03-26 01:24:01 +0700

    Initial commit

---
+++ lmsAPI/Controllers/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace lmsAPI.Controllers
+{
+    internal class Response
+    {
+        public string Status { get; internal set; }
+        public string ErrorCode { get; internal set; }
+        public string ErrorMessage { get; set; }
+        
+        
+    }
+}
