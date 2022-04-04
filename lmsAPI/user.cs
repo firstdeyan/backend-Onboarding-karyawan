1eea12b31338bb5fa3ce05bc076ca18dbb99d0a1
firstdeyan <firstdeyan.septiana.tif19@polban.ac.id>
2022-04-04 20:47:56 +0700

    Initial commit

--- lmsAPI/user.cs
+++ lmsAPI/user.cs
@@ -4,14 +4,16 @@ namespace lmsAPI
 {
     public class user
     {
+
         [Key]
         public string email { get; set; } = string.Empty;
         public string name { get; set; } = string.Empty;
-        public byte[]? passwordHash { get; set; }
-        public byte[]? passwordSalt { get; set; }
         [JsonIgnore]
-        public roles? role_ { get; set; }
-        public job_titles? jobtitle_ { get; set; }
+        public byte[]? passwordHash { get; set; }
+        [JsonIgnore]
+        public byte[]? passwordSalt { get; set; }
+        public roles role_ { get; set; }
+        public job_titles jobtitle_ { get; set; }
         public string gender { get; set; } = string.Empty;
         public DateTime birthdate { get; set; }
         public string phone_number { get; set; } = string.Empty;
