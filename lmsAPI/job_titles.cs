37862e97162fd21e61d828306510bbce6c9aeaf0
firstdeyan <firstdeyan.septiana.tif19@polban.ac.id>
2022-04-04 23:43:47 +0700

    Initial commit

--- lmsAPI/job_titles.cs
+++ lmsAPI/job_titles.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace lmsAPI
 {
     public class job_titles
     {   
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
         public string jobtitle_name { get; set; } = string.Empty;
         public string jobtitle_description { get; set;} = string.Empty;
