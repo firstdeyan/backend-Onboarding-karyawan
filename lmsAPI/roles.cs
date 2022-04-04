ac74602b3168ecb4b07776e4a5dc36ddb03e8936
firstdeyan <firstdeyan.septiana.tif19@polban.ac.id>
2022-04-04 23:44:20 +0700

    Initial commit

--- lmsAPI/roles.cs
+++ lmsAPI/roles.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace lmsAPI
 {
     public class roles
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
         public string role_name { get; set; } = string.Empty;
         public string role_description { get; set; } = string.Empty;
