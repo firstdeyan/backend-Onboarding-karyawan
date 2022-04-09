a19aff845f29cc3227a320de19465dbf4dd22ff0
firstdeyan <firstdeyan.septiana.tif19@polban.ac.id>
2022-04-09 23:38:34 +0700

    Initial commit

---
+++ lmsAPI/Controllers/ActivityCategoryController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+
+
+namespace lmsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "admin,superadmin,user")]
+    [EnableCors]
+    public class ActivityCategoryController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public ActivityCategoryController(DataContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<categories>>> Get()
+        {
+            return Ok(await this.context.categories.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<categories>> Get(int id)
+        {
+            var category = await this.context.categories.FindAsync(id);
+            if (category == null)
+                return BadRequest(new Response
+                {
+                    Status = "error",
+                    ErrorCode = "400",
+                    ErrorMessage = "Activity Category title tidak ditemukan"
+                });
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<categories>>> AddCategory(categories job)
+        {
+            var dbcategory = await this.context.categories.FindAsync(job.id);
+            if (dbcategory != null)
+                return BadRequest(new Response
+                {
+                    Status = "error",
+                    ErrorCode = "400",
+                    ErrorMessage = "Activity Category tidak ditemukan"
+                });
+            this.context.categories.Add(job);
+            await this.context.SaveChangesAsync();
+
+            return Ok(await this.context.categories.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<categories>>> Updatecategory(categories request)
+        {
+            var dbcategory = await this.context.categories.FindAsync(request.id);
+            if (dbcategory == null)
+                return BadRequest(new Response
+                {
+                    Status = "error",
+                    ErrorCode = "400",
+                    ErrorMessage = "Activity Category tidak ditemukan"
+                });
+            dbcategory.category_name = request.category_name;
+            dbcategory.category_description = request.category_description;
+            dbcategory.duration = request.duration;
+
+            await this.context.SaveChangesAsync();
+
+            return Ok(await this.context.categories.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<categories>> Delete(int id)
+        {
+            var dbcategory = await this.context.categories.FindAsync(id);
+            if (dbcategory == null)
+                return BadRequest(new Response
+                {
+                    Status = "error",
+                    ErrorCode = "400",
+                    ErrorMessage = "Activity Category tidak ditemukan"
+                });
+
+            this.context.categories.Remove(dbcategory);
+            await this.context.SaveChangesAsync();
+
+            return Ok(await this.context.categories.ToListAsync());
+        }
+    }
+}
