580397079a6fcb2c069a975a8b429f59be43836e
firstdeyan <firstdeyan.septiana.tif19@polban.ac.id>
2022-05-03 21:00:12 +0700

    untuk api get users by role

---
+++ lmsAPI/Controllers/UsersByRole.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+
+namespace lmsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "admin,superadmin,user")]
+    [EnableCors]
+    public class UsersByRole : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public UsersByRole(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("{role_id}")]
+        public async Task<ActionResult<activities>> GetUsersByRole(int role_id)
+        {
+            var users = await this.context.user.Where(p => p.role_id == role_id).Include(e => e.role_).Include(j => j.jobtitle_).ToListAsync();
+            if (users == null)
+                return BadRequest(new Response
+                {
+                    Status = "error",
+                    ErrorCode = "400",
+                    ErrorMessage = "Tidak ada user"
+                });
+
+            return Ok(users);
+        }
+    }
+}
