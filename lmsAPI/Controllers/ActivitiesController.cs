d9ab46016900e52ffe801439537135a1d33bdf1c
firstdeyan <firstdeyan.septiana.tif19@polban.ac.id>
2022-05-22 23:04:35 +0700

    add column photo table admin and user

--- lmsAPI/Controllers/ActivitiesController.cs
+++ lmsAPI/Controllers/ActivitiesController.cs
@@ -61,7 +61,12 @@ namespace lmsAPI.Controllers
         {
             var dbactivity = await this.context.activities.FindAsync(request.id);
             var category = await this.context.categories.FindAsync(request.category_id);
-            if (request.files != null && request.files.Count() > 0)
+            if (request.files == null)
+            {
+                string url = "api/ShowImage/noimage.png";
+                activities.cover = url;
+            }
+            else if (request.files != null && request.files.Count() > 0)
             {
                 foreach (var file in request.files)
                 {
@@ -71,17 +76,8 @@ namespace lmsAPI.Controllers
                         var path = Path.Combine(Directory.GetCurrentDirectory(), "../lmsAPI/File", Filename);
                         var stream = new FileStream(path, FileMode.Create);
                         file.CopyToAsync(stream);
-                        if (request.files == null)
-                        {
-                            string url = "api/ShowImage/noimage.png";
-                            activities.cover = url;
-                        }
-                        else
-                        {
-                            string url = "api/ShowImage/" + Filename;
-                            activities.cover = url;
-                        }
-
+                        string url = "api/ShowImage/" + Filename;
+                        activities.cover = url;
                     }
                 }
             }
@@ -108,7 +104,12 @@ namespace lmsAPI.Controllers
                     ErrorCode = "400",
                     ErrorMessage = "Activity tidak ditemukan"
                 });
-            if (request.files != null && request.files.Count() > 0)
+            if (request.files == null)
+            {
+                string url = "api/ShowImage/noimage.png";
+                dbactivity.cover = url;
+            }
+            else if (request.files != null && request.files.Count() > 0)
             {
                 foreach (var file in request.files)
                 {
@@ -118,17 +119,8 @@ namespace lmsAPI.Controllers
                         var path = Path.Combine(Directory.GetCurrentDirectory(), "../lmsAPI/File", Filename);
                         var stream = new FileStream(path, FileMode.Create);
                         file.CopyToAsync(stream);
-                        if (request.files == null)
-                        {
-                            string url = "api/ShowImage/noimage.png";
-                            dbactivity.cover = url;
-                        }
-                        else
-                        {
-                            string url = "api/ShowImage/" + Filename;
-                            dbactivity.cover = url;
-                        }
-
+                        string url = "api/ShowImage/" + Filename;
+                        dbactivity.cover = url;
                     }
                 }
             }
