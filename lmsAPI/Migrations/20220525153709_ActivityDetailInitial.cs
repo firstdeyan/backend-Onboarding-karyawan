b3826c5224c663635a4d30166f5b47692bb75440
firstdeyan <firstdeyan.septiana.tif19@polban.ac.id>
2022-05-25 23:13:28 +0700

    post activity details

---
+++ lmsAPI/Migrations/20220525153709_ActivityDetailInitial.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace lmsAPI.Migrations
+{
+    public partial class ActivityDetailInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "activity_details",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    detail_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    detail_desc = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    detail_link = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    detail_type = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    detail_urutan = table.Column<int>(type: "integer", nullable: false),
+                    activity_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_activity_details", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_activity_details_activities_activity_id",
+                        column: x => x.activity_id,
+                        principalTable: "activities",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_activity_details_activity_id",
+                table: "activity_details",
+                column: "activity_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "activity_details");
+        }
+    }
+}
